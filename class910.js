1e4977c337c62524c8d914ab5a362c2cec52b4ab
saidpurtuitionhub <121655497+saidpurtuitionhub@users.noreply.github.com>
2023-01-06 13:00:13 +0600

    Update class910.js

--- class910.js
+++ class910.js
@@ -8,16 +8,41 @@ fetch("class910.json")
         let out = "";
         for (let tutor of class910) {
             out += `
-			<tr>
-				
-				<td>${tutor.name}</td>
-				<td>${tutor.subject}</td>
-				<td>${tutor.phone}</td>
-                <td>${tutor.qualification}</td>
-				<td>${tutor.place}</td>
-				
-			</tr>
-		`;
+        <hr>
+
+        <div class="container">
+        <div class="cards">
+          
+      
+          <div class="cards-box">
+            <h1 class="cards-title">${tutor.name}</h1>
+      
+            <div class="cards-content">
+            <h3 style="color:blue;text-align:center">${tutor.qualification}</h3>
+            <h3>Subject: ${tutor.subject}</h3>
+                <h3>Place: ${tutor.place}</h3>
+                
+                <h4>Minimum expected salary/12 days:${tutor.salary}</h4>
+                <h3>Phone: ${tutor.phone}</h3>
+                </div>
+          </div>
+      
+          <div class="cards-footer">
+            
+            <a href="${tutor.fb}">facebook id link</a>
+            <a href="${tutor.email}">Email</a>
+            <hr>
+          <hr>
+          <h3>Guardian Satisfactory Point: ${tutor.rating}</h3>
+          </div>
+          
+        </div>
+      </div>
+
+
+
+    
+`;
         }
 
         placeholder.innerHTML = out;
